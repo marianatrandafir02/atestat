411da90fa087ef2d12c18c7253bc7f91c1de8f0f
marianatrandafir02 <trandafir.mariana.florentina@cnmvslobozia.ro>
2025-03-28 06:39:23 +0000

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,22 @@
+function calculateResult() {
+    var totalQuestions = 10;
+    var score = 0;
+    
+    for (var i = 1; i <= totalQuestions; i++) {
+        var answer = document.querySelector('input[name="q' + i + '"]:checked');
+        if (answer) {
+            score += parseInt(answer.value);
+        }
+    }
+
+    var resultText = "Ai răspuns corect la " + score + " din " + totalQuestions + " întrebări.";
+    if (score === totalQuestions) {
+        resultText += " Felicitări, ai un scor perfect!";
+    } else if (score >= totalQuestions / 2) {
+        resultText += " Bună treabă, dar mai poți îmbunătăți!";
+    } else {
+        resultText += " Încearcă din nou pentru a învăța mai multe!";
+    }
+
+    document.getElementById("result").innerText = resultText;
+}
